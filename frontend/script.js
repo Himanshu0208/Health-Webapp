e3d0af81cce5888ee56c00e368f95671b24829da
Himanshu <himanshupandey1036@gmail.com>
2023-07-12 23:04:35 +0530

    form created

---
+++ frontend/script.js
@@ -0,0 +1,52 @@
+const fields = ["name", "height", "weight", "problem"];
+let currentFieldIndex = 0;
+let recognition = null;
+
+function speakText(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(utterance);
+}
+
+function startListening() {
+  const fieldId = fields[currentFieldIndex];
+  const labelElement = document.querySelector(
+    `label[for="${fieldId}"]`
+  );
+  const inputElement = document.getElementById(fieldId);
+
+  if (recognition) {
+    recognition.stop();
+  }
+
+  recognition = new (window.SpeechRecognition ||
+    window.webkitSpeechRecognition)();
+  recognition.lang = "en-US";
+
+  recognition.onstart = function () {
+    labelElement.textContent = `Listening for ${labelElement.textContent}...`;
+  };
+
+  recognition.onresult = function (event) {
+    const transcript = event.results[0][0].transcript;
+    inputElement.value = transcript;
+  };
+
+  recognition.onend = function () {
+    labelElement.textContent = `Spoken ${labelElement.textContent}`;
+    currentFieldIndex++;
+
+    if (currentFieldIndex < fields.length) {
+      startListening();
+    } else {
+      currentFieldIndex = 0;
+    }
+  };
+  speakText(fieldId);
+  setTimeout(() => {
+    recognition.start();
+  }, 2000);
+}
+
+if (currentFieldIndex == 0) {
+  speakText("welcome to health webapp");
+}
